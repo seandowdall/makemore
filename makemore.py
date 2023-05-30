e4d938744214881c09e266358d3eba0c7d432e02
Sean Dowdall <seanied1234567@gmail.com>
2023-05-30 12:59:35 +0200

    sampling from the model

--- makemore.py
+++ makemore.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 # Modern Transformers (equivalent to GPT-2)
 
 # USEFUL CODE FOR DOWNLOADING AND OUTPUTTING FILES FROM GITHUB USING REQUESTS
+# ---------------------------------------------------------------------------
 # import requests
 # url = "https://raw.githubusercontent.com/karpathy/makemore/master/names.txt"
 # filename = "names.txt"
@@ -23,6 +24,8 @@ import matplotlib.pyplot as plt
 #     file.write(response.text)
 #
 # print(f"File '{filename}' downloaded and saved successfully.")
+# ------------------------------------------------------------------
+
 
 # load up dataset
 words = open('names.txt', 'r').read().splitlines()
@@ -33,7 +36,7 @@ words = open('names.txt', 'r').read().splitlines()
 
 # To begin we want to build a bigram langauge model. In this we will only be looking at 2 characters at a time
 # we only look at one character that we are given, and we are trying to predict the next character in a sequence
-# e.g. what character is likely ot follow 'r'
+# e.g. what character is likely to follow 'r'
 # very simple but great way to start
 
 b = {}  # dictionary 'b' to maintain counts for every one of the bigrams
@@ -52,34 +55,58 @@ print(sorted(b.items(), key=lambda kv: -kv[
 
 # it will actually be significantly more convenient for us to keep this information in a 2d array instead of a python dictionary
 # we are going to store this information in a 2d array and the rows are going to be the 1st character and the columns will be the second character
-# each entry in this 2d array will tell us how ofter that 2nd char follows the 1st char in the dataset
+# each entry in this 2d array will tell us how often that 2nd char follows the 1st char in the dataset
 # to do this we will use pytorch - deep learning nn framework and torch.tensor which allows us to create multidimensional arrays and manipulate them very efficiently
 
-N = torch.zeros((28, 28), dtype=torch.int32)  # 28 * 28 array of zeros
+N = torch.zeros((27, 27), dtype=torch.int32)  # 28 * 28 array of zeros
 
 chars = sorted(list(set(''.join(words))))
-stoi = {s:i for i,s in enumerate(chars)}
-stoi['<S>'] = 26
-stoi['<E>'] = 27
+stoi = {s: i + 1 for i, s in enumerate(chars)}  # nwo a starts at 1 'a': 1,'b': 2 ........'z':26, '.':0
+stoi['.'] = 0
+# first thing we need to do before visualising the data is invert the array above
+itos = {i: s for s, i in stoi.items()}  # dont need to chnage this as it is just a reverse mapping
 
 for w in words:
-    chs = ['<S>'] + list(w) + [
-        '<E>']  # created a special array here (characters), hallucinated a special start token + 'W' (string 'emma') + end special token
+    chs = ['.'] + list(w) + [
+        '.']  # created a special array here (characters), hallucinated a special start token + 'W' (string 'emma') + end special token
     for ch1, ch2 in zip(chs, chs[1:]):
         ix1 = stoi[ch1]
         ix2 = stoi[ch2]
         N[ix1, ix2] += 1
 
-# first thing we need to do before visualising the data is invert the array above
-itos = {i:s for s, i in stoi.items()}
-
 # for visualizing in a jupyter notebook
 plt.figure(figsize=(16, 16))
 plt.imshow(N, cmap='Blues')
-for i in range(28):
-    for j in range(28):
+for i in range(27):
+    for j in range(27):
         chstr = itos[i] + itos[j]
         plt.text(j, i, chstr, ha="center", va="bottom", color='gray')
         plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
 plt.axis('off');
 
+# row of counts of all the first letters (x-axis)
+# Y axis is all the letters that end a sentence
+# now we follow the counts and start sampling for the model
+
+print(N[0,
+      :])  # indexes into the zeroth row, and colon grabs all the columns  # OUTPUT; tensor([   0, 4410, 1306, 1542, 1690, 1531,  417,  669,  874,  591, 2422, 2963,
+# 1572, 2538, 1146,  394,  515,   92, 1639, 2055, 1308,   78,  376,  307,
+#  134,  535,  929], dtype=torch.int32)
+
+# THESE are the counts, and now we would like to sample from this: so we convert this to probabilities
+# so we create a probability vector
+p = N[0].float()  # int converted to float so that we can normalise these counts
+p = p / p.sum()  # this creates a vector of smaller numbers (now we have probabilities) - p.sum() is now 1 - its is a proper probability distribution. Gives us the probability for any giuven character to be the first character of a word
+print(p)  # OUTPUT: tensor([0.0000, 0.1377, 0.0408, 0.0481, 0.0528, 0.0478, 0.0130, 0.0209, 0.0273,
+# 0.0184, 0.0756, 0.0925, 0.0491, 0.0792, 0.0358, 0.0123, 0.0161, 0.0029,
+# 0.0512, 0.0642, 0.0408, 0.0024, 0.0117, 0.0096, 0.0042, 0.0167, 0.0290])
+
+# now we can try to sample from these distributions
+# we will use torch.multinomial - returns samples from the multinomial probability distributions
+# in other words: you give me probabilities, I give you integers which are sampled according to the probability distribution
+g = torch.Generator().manual_seed(214783647)
+p = torch.rand(3, generator=g)
+p = p / p.sum()
+print(p)
+
+torch.multinomial(p, num_samples=20, replacement=True, generator=g)
