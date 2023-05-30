0e2ed4cd5c88a2eecaee294838c7e53a70e488de
Sean Dowdall <seanied1234567@gmail.com>
2023-05-30 11:39:55 +0200

    counting bigrams in a python dictionary

--- makemore.py
+++ makemore.py
@@ -23,17 +23,25 @@
 
 # load up dataset
 words = open('names.txt', 'r').read().splitlines()
-print(words[: 10])
-print(len(words))
-print(min(len(w) for w in words))
-print(max(len(w)for w in words))
+# print(words[: 10])
+# print(len(words))
+# print(min(len(w) for w in words))
+# print(max(len(w)for w in words))
 
 # To begin we want to build a bigram langauge model. In this we will only be looking at 2 characters at a time
-# we only look at one character that we are given and we are trying to predict the next character in a sequence
+# we only look at one character that we are given, and we are trying to predict the next character in a sequence
 # e.g. what character is likely ot follow 'r'
 # very simple but great way to start
 
-for w in words[:3]:
-    chs = ['<S>'] + list(w) + ['<E>']
-    for ch1, ch2 in zip(chs,  chs[1:]):
-        print(ch1, ch2)
+b = {}  # dictionary 'b' to maintain counts for every one of the bigrams
+# each 'w' here is an individual word (a string)
+for w in words:
+    chs = ['<S>'] + list(w) + [
+        '<E>']  # created a special array here (characters), hallucinated a special start token + 'W' (string 'emma') + end special token
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        b[bigram] = b.get(bigram, 0) + 1  # add up all bigrams and count hwo often they will occur
+        # print(ch1, ch2)
+print(sorted(b.items(), key=lambda kv: -kv[
+    1]))  # returns the tuples that occurred most frequently in descending order. E.g. ('n', '<E>'), 6763 (n was the last letter 6736 times)
+# individual counts that we achieve over the entire dataset
